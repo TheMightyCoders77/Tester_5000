2204955f8718289dabc3ec1ce88205c898ca2d37
20109 <20109@192.168.1.12>
2022-03-18 00:26:24 +0200

    Whatever & Whatever2

---
+++ Tester_5000/src/Hend/Hoseiny/Whatever.java
@@ -0,0 +1,9 @@
+package Hend.Hoseiny;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Whatever {
+ public static void main(String[] args) {
+	System.out.println("whatever");
+}
+}
---
+++ Tester_5000/src/Hend/Hoseiny/Whatever2.java
@@ -0,0 +1,7 @@
+package Hend.Hoseiny;
+
+public class Whatever2 {
+	public static void main(String[] args) {
+		System.out.println("ana Hend");
+	}
+}
