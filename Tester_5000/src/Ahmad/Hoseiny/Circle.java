b6822eed0d5fea3a955e1f6aad5495b0791a441e
20109 <20109@192.168.1.12>
2022-03-17 23:37:52 +0200

    Cicle & Point classes

---
+++ Tester_5000/src/Ahmad/Hoseiny/Circle.java
@@ -0,0 +1,7 @@
+package Ahmad.Hoseiny;
+
+public class Circle {
+	public static void main(String[] args) {
+		System.out.println("Hey!");
+	}
+}
---
+++ Tester_5000/src/Ahmad/Hoseiny/Point.java
@@ -0,0 +1,7 @@
+package Ahmad.Hoseiny;
+
+public class Point {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
